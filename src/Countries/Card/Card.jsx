ddca1aa72604c0e21e13cf13fe916e4e50bd6419
Venkat <venkatsaimane@gmail.com>
2024-01-30 22:37:24 +0530

    Added Search

--- src/Countries/Card/Card.jsx
+++ src/Countries/Card/Card.jsx
@@ -2,7 +2,7 @@ import styles from "./Card.module.css";
 
 export default function Card({ cardData }) {
   return (
-    <div className={styles.card}>
+    <div className={styles.countryCard}>
       <img
         src={cardData.flags.png}
         alt={cardData.flags.alt}
